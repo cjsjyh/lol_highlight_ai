6f5e502a067eabed534be8b944cdcb0da056cfbe
lol <gomataejoon@gmail.com>
2020-07-20 20:43:54 +0900

    Fix util.py and add gitignore

--- preprocessing/ingame_classifier/util.py
+++ preprocessing/ingame_classifier/util.py
@@ -9,7 +9,7 @@ def get_filenames(path):
       break
   return f
 
-def get_game_info(full_name):
+def get_game_info(highlight_path, full_name):
   full_name_split = full_name.split('.')
   file_ext = full_name_split[1]
   file_name = full_name_split[0]
@@ -24,14 +24,23 @@ def get_game_info(full_name):
   for i in range(1, int(len(file_name_split)/2)+1):
     team1 = file_name_split[i*2 - 1]
     team2 = file_name_split[i*2]
-    
-    filtered_list = glob.glob(f"{game_date}_{team1}_{team2}*")
+    print(game_date, team1, team2)
+    # Find highlight videos
+    filtered_list = glob.glob(f"{highlight_path}{game_date}_{team1}_{team2}*")
+    if (len(filtered_list) == 0):
+        # Try with reverse order
+        filtered_list = glob.glob(f"{highlight_path}{game_date}_{team2}_{team1}*")
+        if(len(filtered_list) == 0):
+            return None
     last_set_num = 0
     for name in filtered_list:
       if (name == full_name):
         break
       else:
+        name = name.split('/')[-1]
         name_split = name.split('_')
+        print(name)
+        print(name_split)
         set_num = int(name_split[3])
         if( set_num > last_set_num):
           last_set_num = set_num
@@ -44,4 +53,4 @@ def get_game_info(full_name):
   return game_info
 
 
-print(get_game_info('20200628_AF_DRX_SKT_GEN.mp4'))
+print(get_game_info('../../../tj/2020_spring_highlight_video/','20200305_AF_T1_SB_GEN.mp4'))
