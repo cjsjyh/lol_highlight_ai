69716d1f4670a3a9f5b9bb860e0a8ef6945b338c
Junsoo Choi <jyhckb@naver.com>
2020-09-15 15:39:02 +0900

    modify test_and_val

--- model/test_and_val.sh
+++ model/test_and_val.sh
@@ -16,9 +16,9 @@ echo -n "set name of result_file : "
 read RESULTDIR
 
 
-python3 main.py -t -m $MODEL > $LOGNAME.log
-python3 main.py -m $MODEL > evaluation.$MODEL.$LOGNAME.log
 mkdir result/result_$RESULTDIR
+python3 main.py -t -m $MODEL > result/result_$RESULTDIR/$LOGNAME.log
+python3 main.py -m $MODEL > result/result_$RESULTDIR/evaluation.$MODEL.$LOGNAME.log
 mv data/models/$H5* result/result_$RESULTDIR
 mv data/results.txt result/result_$RESULTDIR
 mv data/splits/$H5* result/result_$RESULTDIR
